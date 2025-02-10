59eca59e108fb650ea70f36952ebe6c8c25623e6
Anubhav Nair <anubhavnair023@gmail.com>
2025-02-11 00:17:32 +0530

    complete api with welcome page

--- server.js
+++ server.js
@@ -8,19 +8,22 @@ config({ path: ".env" });
 import cors from "cors";
 const PORT = process.env.PORT || 5000;
 const app = express();
-
-
-app.use(express.json());
 app.use(cors({
     origin: "*",
     credentials: true,
     methods: ["GET","POST","PUT","PATCH","DELETE"]
 }));
 connectDb();
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+app.set('view-engine', 'ejs');
+app.use(express.static(path.join(path.resolve(),'public')));
 app.use(express.json())
 
 //Contact Routes
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+})
 app.use("/api/contacts",ContactRouter);
 
 app.listen(PORT, () => {
